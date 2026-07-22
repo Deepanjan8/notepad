ecacfa6331f8690a8931fb054951e84db7544b5a
Deepanjan8 <deepanjanbiswas353@gmail.com>
2026-07-22 11:02:26 +0000

    fix: remove unnecessary hilt plugin and kapt compiler from core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.hilt)
 }
 
 android {
@@ -73,7 +72,6 @@ dependencies {
 
     // Hilt
     api(libs.hilt.android)
-    kapt(libs.hilt.compiler)
 
     // Testing
     testImplementation(libs.junit)
